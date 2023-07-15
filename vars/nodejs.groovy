200fd19d98b93bfc27821e13f60bc8d47caaade0
nagababu <nag4c1@gmail.com>
2023-07-15 13:53:26 +0530

    frontend code

---
+++ vars/nodejs.groovy
@@ -0,0 +1,48 @@
+def call() {
+    pipeline {
+        agent {
+            node { label 'workstation' }
+        }
+
+        stages {
+            stage ('build') {
+                steps {
+                    sh 'echo build'
+                    sh 'npm install'
+                }
+            }
+            stage ('test') {
+                steps {
+                    sh 'echo test'
+                    // sh 'npm test'
+
+                }
+            }
+            stage ('Code analysis') {
+                steps {
+                    sh 'echo Code analysis'
+                    //sh 'sonar-scanner -Dsonar.host.url=http://172.31.89.167:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=cart'
+
+
+                }
+            }
+            stage ('Security scans') {
+                steps {
+                    sh 'echo Security scans'
+
+                }
+            }
+            stage ('Publish an artifactory') {
+                when {
+                    expression { env.TAG_NAME ==~ ".*" }
+                }
+                steps {
+                    sh 'echo Publish an artifactory for tag'
+                    sh 'env'
+
+                }
+            }
+        }
+    }
+
+}
